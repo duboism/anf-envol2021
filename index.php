f4d7ed8ce9b6c0df8959fda2b8bfc27834909824
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2021-08-16 07:27:10 +0200

    Main page to run slideshows

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+$f = isset($_SERVER['QUERY_STRING'])
+  ? explode('=', $_SERVER['QUERY_STRING'])[1]
+  : 'html';
+
+switch ($f) {
+  case 'html':
+    $t = 'Les fondamentaux du HTML5';
+    $l = 'html';
+    break;
+  case 'http':
+    $t = 'Le protocole HTTP';
+    $l = 'html';
+    break;
+  case 'css':
+    $t = 'Les fondamentaux de CSS3';
+    $l = 'css';
+    break;
+  case 'js':
+    $t = 'JavaScript pour le Web';
+    $l = 'js';
+    break;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo($t) ?> | ANF ENVOL 2021</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="./layout.css" />
+  </head>
+  <body>
+    <textarea id="source"></textarea>
+    <script src="https://remarkjs.com/downloads/remark-latest.min.js"></script>
+    <script>
+      var slideshow = remark.create({
+        sourceUrl: './files/<?php echo($f); ?>.md',
+        highlightLanguage: '<?php echo($l); ?>',
+        highlightStyle: 'monokai',
+      });
+    </script>
+  </body>
+</html>
