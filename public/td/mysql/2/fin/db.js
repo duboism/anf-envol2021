a2cba460b91b28c7fe53ee2374d2e82a2436ccf7
Alexandre Roulois <alexandre.roulois@u-paris.fr>
2021-10-18 17:01:17 +0200

    Node + MySQL TD1

---
+++ public/td/mysql/2/fin/db.js
@@ -0,0 +1,16 @@
+// db.js
+
+const mysql = require('mysql2/promise');
+const config = require('../private/config');
+
+// create the connection to database
+async function query(sql, params) {
+  // create the connection
+  const connection = await mysql.createConnection(config);
+  // query database
+  const [rows, fields] = await connection.execute(sql, params);
+  
+  return rows;
+}
+
+module.exports = { query };
