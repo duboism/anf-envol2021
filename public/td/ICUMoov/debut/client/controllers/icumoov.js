f318c93a8f0c4d882ad0ee2045244e6e1a762eb2
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 21:51:10 +0100

    TD Arduino - Étape 4 : enregistrer les mesures envoyées par la plateforme Arduino - partie 1
    On définit les routes qui interrogent le serveur Arduino et la fonction
    pour cela (getData).

--- public/td/ICUMoov/debut/client/controllers/icumoov.js
+++ public/td/ICUMoov/debut/client/controllers/icumoov.js
@@ -1,4 +1,4 @@
-// Connection à MongoDB
+// Connection à MongoDB + requêtes au serveur arduino
 
 const mongoose = require('mongoose');
 
@@ -10,3 +10,34 @@ mongoose.connect(uri, {
 })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const net = require('net');
+
+const ARDUINO_PORT = 9999;
+
+const getData = () => {
+    return (req, res) => {
+        // Recupère le paramètre cmd (méthode à interroger)
+        const cmd = req.params.cmd;
+        // Ouvre une socket vers le serveur
+        const socket = net.connect({ port: ARDUINO_PORT });
+        // Écrit la commande et ferme
+        socket.write(cmd);
+        socket.end();
+        // Fonction de rappel
+        socket.on('data', (data) => {
+            const value = data.toString().substr(4);
+            res.render(
+                'data',
+                {
+                    type: cmd,
+                    data: value
+                }
+            );
+        });
+    };
+};
+
+module.exports = {
+    getData
+};
--- public/td/ICUMoov/debut/client/routes/index.js
+++ public/td/ICUMoov/debut/client/routes/index.js
@@ -9,14 +9,7 @@ router.get('/', function(req, res, next) {
     res.redirect('/data');
 });
 
-/* GET /light: enregistre la valeur de la luminosité. */
-router.get('/light', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-/* GET /temperature: enregistre la valeur de la temperature. */
-router.get('/temperature', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+/* GET /:cmd: enregistre la valeur de la luminosité (si :cmd = light) ou de la temperature (si :cmd = temperature). */
+router.get('/:cmd', icumoov.getData());
 
 module.exports = router;
