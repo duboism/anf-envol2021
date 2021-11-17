54ecd18e98b32c9d3ebb600ae47f9c58a81108d3
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 12:03:19 +0100

    2ème Partie - Étape 1 : paramétrer les options de connexion

--- public/td/node/1/debut/client.js
+++ public/td/node/1/debut/client.js
@@ -1,1 +1,12 @@
 // client.js
+
+// module http
+const http = require('http');
+
+// options de connexion
+const options = {
+    hostname: 'localhost',
+    port: 3000,path: '/inscription',
+    method: 'POST'
+}
+
