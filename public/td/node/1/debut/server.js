f94dee942ef66ec402c5b3c66b4a463326674786
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 10:50:13 +0100

    Étape 1 : configurer un serveur prêt à écouter

--- public/td/node/1/debut/server.js
+++ public/td/node/1/debut/server.js
@@ -1,1 +1,22 @@
 // server.js
+
+// On utilise le port 3001 pour ne pas devoir stopper le serveur qui affiche les cours
+const PORT = 3001;
+
+// Modules requis
+const http = require('http');
+const fs = require('fs');
+const stream = require('stream');
+
+const server = http.createServer(
+    (request, response) => {
+        // Affiche la page d'accueil
+        let index = fs.createReadStream('./index.html');
+        stream.pipeline(index, response, (error) => {
+            if (error) console.log(error);
+        });
+    }
+)
+
+server.listen(PORT);
+
