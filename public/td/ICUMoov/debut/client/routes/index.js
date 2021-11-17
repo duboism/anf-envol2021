44bf6f99f02cd70f645847cb487ce57c27abb822
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 21:50:46 +0100

    [MISC] Style

--- public/td/ICUMoov/debut/client/routes/index.js
+++ public/td/ICUMoov/debut/client/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// Controller pour connexion à MongoDB 
+// Controller pour connexion à MongoDB
 const icumoov = require('../controllers/icumoov');
 
 /* GET home page: redirige vers /data. */
