a067c11db91447ce2940e2b8550241b9e961eecd
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 12:12:21 +0100

    2ème Partie - Étape 2 : définir les informations à envoyer

--- public/td/node/1/debut/client.js
+++ public/td/node/1/debut/client.js
@@ -10,3 +10,18 @@ const options = {
     method: 'POST'
 }
 
+// Donnée d'inscription
+const data = {
+    nom: "Roulois",
+    prenom: "Alexandre",
+    email: "alexandre.roulois@u-paris.fr",
+    statut: "i",
+    affiliation: "LLF"
+};
+
+// Formate la requête
+paramvals = [];
+for (let k of Object.keys(data))
+    paramvals.push([k, encodeURIComponent(data[k])].join("="));
+query = paramvals.join("&");
+console.log(query);
