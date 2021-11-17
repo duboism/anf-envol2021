387d7e657a4f12f49381a9cc310a1f0ac32d747f
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 11:11:27 +0100

    Étape 2 : servir les différentes routes - partie 1
    On sert des fichiers statiques à partir de la partie chemin de l'URL et
    on définit des routes "spéciales".

--- public/td/node/1/debut/server.js
+++ public/td/node/1/debut/server.js
@@ -4,14 +4,25 @@
 const http = require('http');
 const fs = require('fs');
 const stream = require('stream');
+const url = require('url');
 
 // On utilise le port 3001 pour ne pas devoir stopper le serveur qui affiche les cours
 const PORT = 3001;
 
 const server = http.createServer(
     (request, response) => {
-        // Affiche la page d'accueil
-        let index = fs.createReadStream('./index.html');
+        // Extrait la partie chemin de l'URL
+        let pathname = url.parse(request.url).pathname;
+
+        // routes d'accès: détermine le nom du fichier en fonction du chemin dans l'URL
+        switch (pathname) {
+        case '/': filename = './index.html'; break;
+        case '/inscription': filename = './confirmation.html'; break;
+        default: filename = pathname;
+        }
+
+        // Affiche le fichier
+        let index = fs.createReadStream(filename);
         stream.pipeline(index, response, (error) => {
             if (error) console.log(error);
         });
