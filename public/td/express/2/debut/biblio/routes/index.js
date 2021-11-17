d909e60a47a1d039199204817e37c85061f3ee53
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 18:07:57 +0100

    TD Express 2 - Étape 4 : lister les références bibliographiques - partie 1
    Chargement et affichage basique de la liste des publis.
    
    Note: la partie 3 a été faite dans 349697769037020b2419a6a40299c82e5605f71e

--- public/td/express/2/debut/biblio/routes/index.js
+++ public/td/express/2/debut/biblio/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+// Pour lire la base de données
+const fs = require('fs');
+
+const DBFILE = './private/db.json';
+
 /* GET home page: liste les références bibliographiques. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Références bibliographiques' });
+    console.log("Ça va lire");
+    fs.readFile('./private/db.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        rows = JSON.parse(data);
+    });
+    console.log(rows);
+    const documents = Array();
+    for (doc of rows.response.docs) {
+        documents.push({
+            ref: doc.label_s
+        });
+    }
+    res.render(
+        'index',
+        {
+            title: 'Références bibliographiques',
+            docs: documents.sort(),
+        });
 });
 
 module.exports = router;
