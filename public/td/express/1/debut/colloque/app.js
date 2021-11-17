a139cce40e4fa4337832814eb296ddfeed0d0119
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 15:57:04 +0100

    TD Express.js 1 - Étape 2 : routage des requêtes - partie 2
    Utilisation du modèle 404 pour l'éponyme erreur (et code idoine).

--- public/td/express/1/debut/colloque/app.js
+++ public/td/express/1/debut/colloque/app.js
@@ -28,7 +28,8 @@ app.use('/inscription', inscriptionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    res.status(404);
+    res.render('404');
 });
 
 // error handler
