2b7001a094a5786ad9b8c3561d7f5aa54af3af29
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 14:59:09 +0100

    TD Node 2 - Étape 2 : un middleware pour afficher un fichier HTML

--- public/td/node/2/debut/server.js
+++ public/td/node/2/debut/server.js
@@ -2,11 +2,18 @@
 
 // modules requis
 const connect = require('connect');
+// Notre module
+const utils = require('utils');
 
 PORT = 3001;
 
 // app
 const app = connect();
+
+// middlewares
+app.use('/home', utils.loadFile('./index.html') );
+app.use('/inscription', utils.loadFile('./confirmation.html'));
+app.use(utils.loadFile('./404.html'));
 
 // port d'écoute
 app.listen(PORT);
