8e2ec64079fafdeab18826950201e91da24cbcfd
Mathieu Dubois <mdubois@genoscope.cns.fr>
2021-11-17 20:31:33 +0100

    TD Express 2 - Étape 4 : lister les références bibliographiques - partie 3

--- public/td/express/2/debut/biblio/routes/index.js
+++ public/td/express/2/debut/biblio/routes/index.js
@@ -16,17 +16,38 @@ router.get('/', function(req, res, next) {
         if (err) throw err;
         rows = JSON.parse(data);
         console.log(rows);
-        const documents = Array();
-        for (doc of rows.response.docs) {
-            documents.push({
-                ref: entities.decode(doc.label_s)
-            });
-        }
+        // Groupe par année + decodage
+        // Note: on  garde tout le document
+        const documents = rows.response.docs.reduce(
+            (acc, raw_doc) => {
+                // Décodage des entités HTML
+                raw_doc.ref = entities.decode(raw_doc.label_s);
+
+                // Année de la publi
+                // On ne la convertit pas en entier car on stocke dans un objet
+                // Note: les Map ne semblent pas fonctionner non plus
+                let year = raw_doc.publicationDate_tdate.substring(0, 4);
+                console.log(year);
+
+                // Si l'année n'est pas dans l'accumulateur, on initialise
+                if (!acc[year]) {
+                    acc[year] = [];
+                }
+
+                // Ajoute le document pour l'année
+                acc[year].push(raw_doc);
+
+                return acc;
+            },
+            {}
+        );
+        console.log(documents);
+        // Affichage
         res.render(
             'index',
             {
                 title: 'Références bibliographiques',
-                docs: documents.sort(),
+                docs: documents,
             }
         );
     });
